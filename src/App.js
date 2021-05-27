805b58cf3a9597f2c3ba3597a2966c14535645e6
patyson <philip.tyson.dev@gmail.com>
2021-05-27 08:18:15 -0600

    Refactoring this to be a smaller project just focused on one thing to demonstrate React example in a more complete environ

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 //import './App.css';
+import React, { useEffect, useState } from 'react'
 import Header from './components/Header'
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div>
-        <Header />
-    </div>
-  );
+//function App() {
+//    const [error, setError] = useState(null)
+//    const [isLoaded, setIsLoaded] = useState(false)
+//    const [user, setUser] = useState({})
+
+//    useEffect(() => {
+//        fetchUser()
+//    }, [])
+
+//    //get random user info
+//    const fetchUser = async () => {
+//        const res = await fetch('https://randomuser.me/api')
+//        const data = await res.json()
+//        //don't need info part of json, just the user data 'results'
+//        const userObj = data.results[0]
+//        setIsLoaded(true)
+//        setUser(userObj)
+//        const userName = userObj.name.first
+//    }
+
+
+//    return (
+//        <div>
+//            <Header firstName={user.name.first}/>
+//            <Home />
+//        </div>
+//    );
+//}
+
+class App extends React.Component {
+    componentDidMount() {
+        const fetchUser = async () => {
+            fetch('https://randomuser.me/api')
+            .then(res => res.json())
+            .then(data => this.setState({user: data}))
+        }
+
+    }
+
+    render() {
+        console.log(this)
+       return (
+            <div>
+                <Header firstName={this.name.first}/>
+                <Home />
+            </div>
+        );
+    }
 }
 
 export default App;
